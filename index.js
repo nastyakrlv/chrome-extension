8b4e62e5e93f9dd82791d2f249e681047931c5f1
nastyakrlv <akor0611@gmail.com>
2023-09-15 20:21:32 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+let myLeads = []
+
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const delBtn = document.getElementById("del-btn")
+const tabBtn = document.getElementById("tab-btn")
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+inputBtn.addEventListener("click", 
+function() {
+    if (inputEl.value) {
+        myLeads.push(`https://${inputEl.value}`)
+        inputEl.value = ""
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    }
+    
+    
+})
+
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
+
+delBtn.addEventListener("click", 
+function() {
+    if (confirm("Are you sure you want to delete all?")) {
+        localStorage.clear()
+        myLeads = []
+        ulEl.innerHTML = ""
+    }
+})
+
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
